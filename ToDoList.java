1316829e6e93bc4fc076d0c2b00947161a5ce502
Mustafa Mohamed <mustafazewaidi747@gmail.com>
2026-09-01 16:16:29 +0300

    Add files via upload

---
+++ ToDoList.java
@@ -0,0 +1,15 @@
+package PracticalProject;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+// 6. الكلاس الرئيسي لتشغيل الواجهة
+public class ToDoList {
+    public static void main(String args[]) {
+        AppFrame frame = new AppFrame(); // استدعاء النافذة الرئيسية لتبدأ في العمل والظهور
+    }
+}
